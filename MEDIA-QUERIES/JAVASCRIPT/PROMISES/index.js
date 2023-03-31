625814cc44e235b6c25e93f87109575f061dbeea
xenia11 <dasicksenija@gmail.com>
2023-03-31 12:58:56 +1100

    async func

--- MEDIA-QUERIES/JAVASCRIPT/PROMISES/index.js
+++ MEDIA-QUERIES/JAVASCRIPT/PROMISES/index.js
@@ -9,27 +9,76 @@ const delayedIncDecr = (n, mode, timeout = 2500) => {
                 incN -= 1;
                 resolve(incN);
             }
+            reject(new Error(console.log(`The mode is wrong`)));
         }, timeout);
     });
 };
 
-const validateN = (n) => {
+const validateN = (incN) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (typeof n !== "number") {
+            if (typeof incN !== "number") {
                 reject(new Error("param 1 must be a number"));
             }
-            resolve(n);
+            resolve(incN);
         }, 1000);
     });
 };
 
-delayedIncDecr("xzxv", "inc", 1000)
-    .then((num) => validateN())
+delayedIncDecr(5, "inc", 1000)
+    .then((incN) => validateN())
+    .catch((error) => console.log(error))
     .then((num) => console.log("The new number is", num))
     .catch((err) => console.log(err.message));
 
-delayedIncDecr(9, "decr", 1000).then((num) =>
-    console.log("The new number is", num)
-);
-delayedIncDecr(3, "inc").then((num) => console.log("The new number is", num));
+// const delayedIncDec = (n, mode, timeout = 2500) => {
+//     return new Promise((resolved, rejected) => {
+//         setTimeout(() => {
+//             if (mode === "inc") {
+//                 n++;
+//                 resolved(n);
+//             } else if (mode === "dec") {
+//                 n--;
+//                 resolved(n);
+//             }
+//             if (
+//                 typeof n !== "number" ||
+//                 mode !== "inc" ||
+//                 mode !== "dec" ||
+//                 typeof timeout !== "number"
+//             ) {
+//                 rejected(new Error("Bad Input"));
+//             }
+//         }, timeout);
+//     });
+// };
+
+// const myFilter = (array) => {
+//     return array.filter((item) => item === item.toUpperCase());
+// };
+
+const rememberName = (input) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(input);
+        }, 2000);
+    });
+};
+
+const promises = [];
+promises.push(rememberName("Charise"));
+promises.push(rememberName("Alex"));
+promises.push(rememberName("Hannah"));
+
+const namesP = Promise.all(promises);
+
+namesP.then((names) => console.log(`Hi ${names[0]}, ${names[1]}, ${names[2]}`));
+
+const myAsyncFunc = async () => {
+    return "Hello!";
+};
+const myAsyncError = async () => {
+    throw new Error("oh no");
+};
+myAsyncFunc().then(console.log);
+myAsyncError().catch((e) => console.log(e.message));
